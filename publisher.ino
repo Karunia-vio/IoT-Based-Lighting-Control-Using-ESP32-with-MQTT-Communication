4f4c0a1cd557315e9f5f6d03c29b7411c0f40934
Karunia-vio <kvionita10@gmail.com>
2024-12-07 10:55:51 +0700

    Add files via upload

---
+++ publisher.ino
@@ -0,0 +1,98 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "DIDI";
+const char* password = "1707197711";
+const char* mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int NUM_LAMPS = 3;
+const int BUTTON_PIN[NUM_LAMPS] = {18,2,4};
+int buttonState[NUM_LAMPS] = {HIGH};
+int lastButtonState[NUM_LAMPS] = {HIGH};
+int buttonPressCounter[NUM_LAMPS] = {0};
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  for (int i = 0; i < NUM_LAMPS; i++) {
+    int reading = digitalRead(BUTTON_PIN[i]);
+
+    if (reading != lastButtonState[i]) {
+      
+
+
+
+      if (reading == LOW) {
+        buttonPressCounter[i]++; 
+
+        int brightness = 0;
+        if (buttonPressCounter[i] == 1) {
+          brightness = 0;
+        } else if (buttonPressCounter[i] == 2) {
+          brightness = 100;
+        } else if (buttonPressCounter[i] == 3) {
+          brightness = 50;
+          buttonPressCounter[i] = 0; 
+        }
+
+        publishMessage(i + 1, brightness);
+      }
+    }
+
+    lastButtonState[i] = reading;
+  }
+}
+
+void publishMessage(int lamp, int brightness) {
+  StaticJsonDocument<200> doc;
+  doc["lamp"] = lamp;
+  doc["brightness"] = brightness;
+
+  char buffer[512];
+  serializeJson(doc, buffer);
+  client.publish("lamp_control", buffer);
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32_Publisher")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
---
+++ subscriber.ino
@@ -0,0 +1,79 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "DIDI";
+const char* password = "1707197711";
+const char* mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int NUM_LAMPS = 3;
+const int LED_PIN[NUM_LAMPS] = {18,2,4};
+int lampBrightness[NUM_LAMPS] = {0};
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload, length);
+  
+  int lamp = doc["lamp"];
+  int brightness = doc["brightness"];
+
+  if (lamp >= 1 && lamp <= NUM_LAMPS) {
+    lampBrightness[lamp - 1] = brightness;
+    analogWrite(getPinFromLamp(lamp), brightness * 2.55);
+  }
+}
+int getPinFromLamp(int lamp) {
+  
+  if (lamp >= 1 && lamp <= NUM_LAMPS) {
+    return LED_PIN[lamp - 1];
+  } else {
+    return -1; 
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32_Subscriber")) {
+      Serial.println("connected");
+      client.subscribe("lamp_control");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
